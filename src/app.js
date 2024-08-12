57e997c4013a872b6ac94b2117c4d7ccf10bcc67
taimoor07 <dev.taimoor7@gmail.com>
2024-08-12 13:32:37 -0700

    add env setup

--- src/app.js
+++ src/app.js
@@ -4,16 +4,15 @@ const app = express();
 const customerRouters = require("./routes/customer.js");
 const inventoryRouters = require("./routes/inventory.js");
 const authRouters = require("./routes/auth.js");
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+
+const port = process.env.PORT || 3000;
 
 // Parse JSON payloads
 app.use(express.json());
 
-const mongodbUrl = 'mongodb://localhost:27017/erp';
-const mongodbOptions = {
-}
-
 // Connect to MongoDB
-mongoose.connect(mongodbUrl, mongodbOptions).then(() => {
+mongoose.connect(process.env.DB_URI, {}).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.error('Failed to connect to MongoDB', err);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log("Server is listening on port: ", port)
-})
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
